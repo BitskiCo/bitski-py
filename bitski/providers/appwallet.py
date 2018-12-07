1bd3cb92855b244f6b7d5041c7b60e0c16662c20
Patrick Tescher <patrick@outtherelabs.com>
2018-12-07 14:03:55 -0800

    Add client ID headers

--- bitski/providers/appwallet.py
+++ bitski/providers/appwallet.py
@@ -9,6 +9,7 @@ class AppWalletProvider(Web3HTTPProvider):
     def __init__(self, client_id, client_secret, network_name):
         service_information = ServiceInformation('https://account.bitski.com/oauth2/auth', 'https://account.bitski.com/oauth2/token', client_id, client_secret, ['eth_sign'])
         self.manager = CredentialManager(service_information)
+        self.client_id = client_id
         super().__init__('https://api.bitski.com/v1/web3/' + network_name)
 
     def get_request_headers(self):
@@ -17,7 +18,9 @@ class AppWalletProvider(Web3HTTPProvider):
 
         headers = Web3HTTPProvider.get_request_headers(self)
         headers.update({
-            'Authorization': 'Bearer ' + self.manager._access_token
+            'Authorization': 'Bearer ' + self.manager._access_token,
+            'X-API-Key': self.client_id, 
+            'X-Client-Id': self.client_id,
         })
 
         return headers
